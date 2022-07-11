2532d6e95dff65e32361111e8da72c869234d822
Alex <devtamin@gmail.com>
2022-07-11 09:54:59 -0400

    complete tailwind tutorial in 1 hour

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'pacifico': ["'Pacifico'", 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+}
